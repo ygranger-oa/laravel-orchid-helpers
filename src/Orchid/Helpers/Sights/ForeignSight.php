f59ba4eb9e9a4e5b75edad4d0c44084c9eb6604d
Yannick GRANGER <yannick.granger@stratupvisor.com>
2023-12-04 14:50:01 +0100

    add foreign key sight

---
+++ src/Orchid/Helpers/Sights/ForeignSight.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Manzadey\LaravelOrchidHelpers\Orchid\Helpers\Sights;
+
+use Illuminate\Database\Eloquent\Model;
+use Orchid\Screen\Fields\Label;
+use Orchid\Screen\Layouts\Persona;
+use Orchid\Screen\Sight;
+
+class ForeignSight
+{
+    public static function make($entityName, $propertyName, string $title) : Sight
+    {
+        return Sight::make($entityName, $title ?? attrName($entityName))
+        ->render(function ($model) use ($entityName, $propertyName){
+            return  Label::make()->value(optional($model->$entityName)->$propertyName);
+        });
+    }
+}
